d1faa27a52b55ac918a5531b33735cb3b894ff00
Penelope Yong <penelopeysm@gmail.com>
2026-02-06 19:19:41 +0000

    css

--- components/TopMenu.tsx
+++ components/TopMenu.tsx
@@ -17,18 +17,18 @@ export default function TopMenu({ user }: TopMenuProps) {
     console.log('topMenu user', user);
     return (
         <nav className="flex justify-between items-center p-4 bg-gray-200">
-            <div className="flex gap-4 ml-4">
+            <div className="flex gap-5 ml-4">
                 <Link href="/">Home</Link>
                 <Link href="/projects">All Projects</Link>
                 <Link href="/users">All Users</Link>
-                <Link href="/submit">Submit</Link>
             </div>
 
-            <div className="flex gap-4 mr-4 items-center">
+            <div className="flex gap-5 mr-4 items-center">
                 {user ? (
                     <>
                         <span>Welcome, {user.githubUsername}!</span>
                         <Link href="/my-profile">My Profile</Link>
+                        <Link href="/submit">Submit a Project</Link>
                         <Link href="/api/auth/signout">Logout</Link>
                         <Image src={user.githubPicture} alt="GitHub profile picture" width={32} height={32} className="rounded-full" />
                     </>
